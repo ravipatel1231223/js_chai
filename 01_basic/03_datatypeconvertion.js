73eb0a05a121c253a465e6024298d6ffdf0f8491
RaviPatel <159173766+ravipatel1231223@users.noreply.github.com>
2024-11-19 11:34:51 +0000

    diferent type of data convertion

---
+++ 01_basic/03_datatypeconvertion.js
@@ -0,0 +1,31 @@
+let num="33as"
+console.log(num);
+let no=Number(num)
+console.log(typeof num);
+console.log(typeof no);
+console.log(no);//NaN
+/*
+conversion in Number
+"33">>33
+a33>> NaN
+null>>0
+undefine>>undeine
+true>>1
+conver in bool
+"">>>false
+"efef">>true
+
+*/
+console.log("1"+2);//12
+console.log("1"+2+2);//122
+console.log(null>0);//f
+console.log(null==0);//f
+console.log(null<=0);//t
+console.log(null>=0);//true
+//undifine alsway compare >>> 0
+
+//=== check type with number
+console.log("2"===2);//false
+console.log(2.0===2);//t
+
+
