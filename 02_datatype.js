44cf56635496ca67eef340ce6e1ad12af14e25e8
RaviPatel <159173766+ravipatel1231223@users.noreply.github.com>
2024-11-19 05:16:45 +0000

    config add

---
+++ 02_datatype.js
@@ -0,0 +1,18 @@
+
+"use strict "
+// alert not use bcz we in node not brawoser
+/* premitive 
+number => s^53
+boolean true/false
+sreing " "
+null standalone value
+
+undefine abhi value assign nahi ki hai
+
+symbol uniqueness
+
+no primitive
+object 
+typeof null >> object
+typeof undifine >> undifine
+*/
