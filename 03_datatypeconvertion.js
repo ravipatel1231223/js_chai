0becdb80865696bfaf8567cd13a7b0b61465d7a0
RaviPatel <159173766+ravipatel1231223@users.noreply.github.com>
2024-11-19 11:11:52 +0000

    converstion of datatype

---
+++ 03_datatypeconvertion.js
@@ -0,0 +1,18 @@
+let num="33as"
+console.log(num);
+let no=Number(num)
+console.log(typeof num);
+console.log(typeof no);
+console.log(no);//NaN
+/*
+conversion in Number
+"33">>33
+a33>> NaN
+null>>0
+undefine>>undeine
+true>>1
+conver in bool
+"">>>false
+"efef">>true
+
+*/
