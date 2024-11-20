f918d27b5f56ab5d6e7dd32985b2f7a88aae7023
RaviPatel <159173766+ravipatel1231223@users.noreply.github.com>
2024-11-20 10:31:53 +0000

    summry of datatype

---
+++ datatype_summary.js
@@ -0,0 +1,16 @@
+//premitive >>string number bool null umdefine symbol BigInt
+
+//refrence type>> array object Function
+
+// string >>string
+// number>>number
+// boolean>>boolean
+// symbol>>symbol
+
+// null>>object
+
+// undefine>>undefine
+// array>>array object
+// object>>object
+
+// function>>function object
